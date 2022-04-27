5fa86a8f7f90efe81b793593b97518a4e015b87b
Alexei Karamzin <1999aleks_1999@mail.ru>
2022-04-27 21:45:20 +0300

    container

--- src/SkillsBlock/SkillsBlock.tsx
+++ src/SkillsBlock/SkillsBlock.tsx
@@ -4,6 +4,8 @@ import Comp3 from "./Comp3/Comp3";
 import Comp1 from "./Comp1/Comp1";
 import Comp2 from "./Comp2/Comp2";
 
+
+
 const SkillsBlock = () => {
     return (
         <div className={classes.SkillsBlockRootContainer}>
@@ -12,9 +14,10 @@ const SkillsBlock = () => {
                     <h2>My services</h2>
                 </div>
                 <div className={classes.content}>
-                    <Comp1 />
-                    <Comp2 />
-                    <Comp3 />
+
+                    <Comp1 title={'01. React'}/>
+                    <Comp2  title={'02. Redux'}/>
+                    <Comp3  title={'01. JS/TS'}/>
                 </div>
             </div>
         </div>
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './Main.module.css'
-import Container from '../common/styles/Container.module.css'
+import ContainerStyles from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
-        <div className={Container.mainContainer}>
+        <div className={ContainerStyles.mainContainer}>
             <div className={classes.rootBlock}>
                 <h1 className={classes.rootText}>Hello,<br/>I am Aleksei Karamzin<br/>frontend developer</h1>
                 <div className={classes.photo}>photo</div>
